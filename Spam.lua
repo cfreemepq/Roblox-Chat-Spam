3e9e423896657c60ab3de9e5d7325dce9415e8bd
casanova <107594088+cfreemepq@users.noreply.github.com>
2022-06-24 20:14:47 +0200

    Update Spam.lua

--- Spam.lua
+++ Spam.lua
@@ -4,4 +4,4 @@
 while true do wait(1) 
  
 local A_1 = "casanova on top" local A_2 = "check my others repositories" --You can edit this, A_1 = your first message and A_2 the second
-local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest Event:FireServer(A_1, A_2)
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest Event:FireServer(A_1, A_2) end
